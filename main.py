e54701b358f10bc80ebecd27dddb4ee7bd5e86f0
Konstantin.L <llaskot@gmail.com>
2025-05-28 02:08:23 +0300

    finish

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+from ui.main_window import main
+import flet as ft
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    ft.app(target=main)
